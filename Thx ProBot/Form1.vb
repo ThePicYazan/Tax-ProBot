9cd8e6010f0f61088db9cb6b752841938e43fb40
YAZAN <117681399+ThePicYazan@users.noreply.github.com>
2023-03-27 03:44:13 +0300

    Add files via upload

---
+++ Thx ProBot/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class tax
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim time As String = TextBox1.Text
+        time = time.Replace("k", "0000").Replace("m", "00000")
+        Dim tax As Integer = Math.Floor(Double.Parse(time) * (20) / (19) + (1))
+        MessageBox.Show(tax)
+    End Sub
+End Class
---
+++ Thx ProBot/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Thx ProBot")>
+<Assembly: AssemblyDescription("YAZAN#1411")>
+<Assembly: AssemblyCompany("YAZAN#1411")>
+<Assembly: AssemblyProduct("Thx ProBot")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("YAZAN#1411")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a51d7420-30d0-432a-8c64-82a878ad4996")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
